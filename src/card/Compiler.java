4b29c7352f296da1f5c35ca2c67168be43c74a2a
chemry <16097448d@connect.polyu.hk>
2018-04-16 00:53:20 +0800

    add compiler

--- src/card/Compiler.java
+++ src/card/Compiler.java
@@ -1,29 +1,32 @@
 
 package card;
+
 import card.parser.*;
 import card.lexer.*;
 import card.node.*;
+
 import java.io.*;
 import java.util.Scanner;
 
 public class Compiler {
- public static void main(String[] args) {
+    public static void main(String[] args) {
 //  Scanner in = new Scanner(System.in);
 //  String file = in.next();
 //  System.out.println(file);
-  try {
-   //Createa Parser instance.
-   Parser p = new Parser(
-    new Lexer(
-     new PushbackReader(
-      new BufferedReader(
-       new FileReader(args[0])), 1024)));
-   //Parsetheinput.
-   Start tree = p.parse();
-   System.out.println("Good");
-  } catch (Exception e) {
-   System.out.println(e.getMessage());
-  }
- }
+        try {
+            //Createa Parser instance.
+            Parser p = new Parser(
+                    new Lexer(
+                            new PushbackReader(
+                                    new BufferedReader(
+                                            new FileReader(args[0])), 1024)));
+            //Parsetheinput.
+            Start tree = p.parse();
+            tree.apply(new SemanticAnalyzer());
+            System.out.println("Good");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
 
