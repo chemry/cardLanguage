39e0496c3fa3637f2645ca5c7ca8f52466e31ba0
chemry <16097448d@connect.polyu.hk>
2018-04-16 00:51:37 +0800

    add semantic Analyzer

--- src/card/SemanticAnalyzer.java
+++ src/card/SemanticAnalyzer.java
@@ -1,8 +1,28 @@
 package card;
 
-/**
- * Created by CHENG Mingrui on 2018/4/16.
- */
-public class SemanticAnalyzer {
+import card.node.*;
+import card.analysis.*;
+
+public class SemanticAnalyzer extends DepthFirstAdapter {
+
+    public void caseTIdentifier(TIdentifier node){
+        System.out.println("ident node seen: " + node);
+    }
+
+    /*public void outAAssignAssignrest(AAssignAssignrest node){
+        System.out.println("AAssignAssignrest node: " + node);
+        System.out.println("AAssignAssignrest right identifier seen: " + node.getIdentifier());
+    }
+
+    public void outADeclareStatement(ADeclareStatement node){
+        System.out.println("ADeclareStatement node: " + node);
+        System.out.println("ADeclareStatement Identifier node: " + node.getIdentifier());
+    }
+
+    public void outAAssignStatement(AAssignStatement node){
+        System.out.println("AAssignStatement node: " + node);
+        System.out.println("AAssignStatement node: " + node.getIdentifier());
+    }*/
+
 
 }
