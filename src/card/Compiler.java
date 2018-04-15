7b61b6c9b2041bbe4558dce1504348e07f903197
chemry <16097448d@connect.polyu.hk>
2018-04-16 00:46:49 +0800

    Initial

---
+++ src/card/Compiler.java
@@ -0,0 +1,29 @@
+
+package card;
+import card.parser.*;
+import card.lexer.*;
+import card.node.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Compiler {
+ public static void main(String[] args) {
+//  Scanner in = new Scanner(System.in);
+//  String file = in.next();
+//  System.out.println(file);
+  try {
+   //Createa Parser instance.
+   Parser p = new Parser(
+    new Lexer(
+     new PushbackReader(
+      new BufferedReader(
+       new FileReader(args[0])), 1024)));
+   //Parsetheinput.
+   Start tree = p.parse();
+   System.out.println("Good");
+  } catch (Exception e) {
+   System.out.println(e.getMessage());
+  }
+ }
+}
+
---
+++ src/card/SemanticAnalyzer.java
@@ -0,0 +1,8 @@
+package card;
+
+/**
+ * Created by CHENG Mingrui on 2018/4/16.
+ */
+public class SemanticAnalyzer {
+
+}
